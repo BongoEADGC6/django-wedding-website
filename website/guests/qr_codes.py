879605234775fadd255e14d78a2477c3c5e73560
Cliff H <bongo@haguest.com>
2019-05-06 15:40:48 -0400

    Change qr code writing

--- website/guests/qr_codes.py
+++ website/guests/qr_codes.py
@@ -19,13 +19,17 @@ def gen_qr_code(url):
     img = qr.make_image(fill_color="black", back_color="white")
     return img
 
-def create_qr_codes(output_file):
+def create_qr_codes(output_dir):
     to_send_to = Party.in_default_order().filter(is_invited=True, invitation_sent=None).exclude(is_attending=False)
     table_data = str()
     for party in to_send_to:
         print("Creating QR for: {} - {}".format(party.name, party.invitation_id))
         rsvp_url = reverse('invitation', args=[party.invitation_id])
         qr_img = gen_qr_code(rsvp_url)
+        qr_img_path = "{}/img/{}.png".format(output_dir, party.invitation_id)
+        qr_img_static ="img/{}.png".format(party.invitation_id)
+        with open(qr_img_path) as image:
+            image.write(qr_img)
         body_text = """
             Please scan the code above with your smart phone or<br/>
             navigate to the following link to submit your RSVP<br/>
@@ -33,10 +37,12 @@ def create_qr_codes(output_file):
             """.format(rsvp_url)
         table_entry = """
               <tr>
-                <td class="tg-0lax">{}</td>
+              </tr>
+              <tr>
+                <td class="tg-0lax"><img src="{}"></td>
                 <td class="tg-0lax">{}</td>
               </tr>
-            """.format(qr_img, body_text)
+            """.format(qr_img_static, body_text)
         table_data += "{}\n".format(table_entry)
     # Build full html page for printing
     table_text = """
@@ -52,6 +58,6 @@ def create_qr_codes(output_file):
         </body>
         </html>
         """.format(table_text)
-    with open(output_file, "w") as f:
+    with open("{}/qr.html".format(output_dir), "w") as f:
             f.write(html_text)
 
