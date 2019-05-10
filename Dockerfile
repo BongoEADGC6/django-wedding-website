984c521bf0d2889aa3c01ef56fc2cc051c459222
Cliff Hults <cliff.hults@gmail.com>
2019-05-10 07:39:41 -0400

    Updated staticfiles config

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ RUN mkdir /code
 ADD . /code/
 RUN pip install -r /code/requirements.txt
 WORKDIR /code/website
+RUN python manage.py collectstatic --noinput
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- website/bigday/settings.py
+++ website/bigday/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT =  'static_root'
+STATIC_ROOT =  os.path.join('bigday', 'static'),
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join('bigday', 'static'),
+    '/code/static'
 )
 
 
