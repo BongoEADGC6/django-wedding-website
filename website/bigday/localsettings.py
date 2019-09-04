d94529bcff0a5cfe3677e555517dd26b8c2f9622
Cliff Hults <cliff.hults@gmail.com>
2019-09-04 07:52:25 -0400

    Updated meta

--- website/bigday/localsettings.py
+++ website/bigday/localsettings.py
@@ -3,6 +3,6 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mrmrshults@gmail.com'
-EMAIL_HOST_PASSWORD = 'dpfsekxxykfncvle'
+EMAIL_HOST_USER = 'examples@gmail.com'
+EMAIL_HOST_PASSWORD = 'e'
 EMAIL_PORT = 587
